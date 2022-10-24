358db139dade993e1bae8a7fd0d28a3e2c71fe26
baptcott28 <baptiste.cottu@ensea.fr>
2022-10-24 16:53:42 +0200

    commi_bapt

--- TP_actionneurs_cottu_jaimes/Core/Src/main.c
+++ TP_actionneurs_cottu_jaimes/Core/Src/main.c
@@ -308,7 +308,7 @@ static void MX_TIM1_Init(void)
     Error_Handler();
   }
   sConfigOC.OCMode = TIM_OCMODE_PWM1;
-  sConfigOC.Pulse = 1228;
+  sConfigOC.Pulse = 1050;
   sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
   sConfigOC.OCNPolarity = TIM_OCNPOLARITY_HIGH;
   sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
@@ -318,7 +318,7 @@ static void MX_TIM1_Init(void)
   {
     Error_Handler();
   }
-  sConfigOC.Pulse = 818;
+  sConfigOC.Pulse = 700;
   if (HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_2) != HAL_OK)
   {
     Error_Handler();
