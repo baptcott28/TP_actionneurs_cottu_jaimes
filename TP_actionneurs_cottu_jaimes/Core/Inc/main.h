b96b0890c103ed5812f1db11adccfc9aefcacbf6
baptcott28 <baptiste.cottu@ensea.fr>
2022-10-24 14:25:51 +0200

    commit bapt

--- TP_actionneurs_cottu_jaimes/Core/Inc/main.h
+++ TP_actionneurs_cottu_jaimes/Core/Inc/main.h
@@ -59,13 +59,13 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
-#define B1_Pin GPIO_PIN_13
-#define B1_GPIO_Port GPIOC
-#define B1_EXTI_IRQn EXTI15_10_IRQn
-#define ISO_reset_Pin GPIO_PIN_3
-#define ISO_reset_GPIO_Port GPIOC
-#define LD2_Pin GPIO_PIN_5
-#define LD2_GPIO_Port GPIOA
+#define BUTTON_Pin GPIO_PIN_13
+#define BUTTON_GPIO_Port GPIOC
+#define BUTTON_EXTI_IRQn EXTI15_10_IRQn
+#define ISO_RESET_Pin GPIO_PIN_3
+#define ISO_RESET_GPIO_Port GPIOC
+#define GREEN_LED_Pin GPIO_PIN_5
+#define GREEN_LED_GPIO_Port GPIOA
 #define T_SWDIO_Pin GPIO_PIN_13
 #define T_SWDIO_GPIO_Port GPIOA
 #define T_SWCLK_Pin GPIO_PIN_14
